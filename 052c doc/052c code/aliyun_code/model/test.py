f483f9127c1e9c91f878505163ea1511f4f125ea
HuiLanAi <310-we-aaa-1@163.com>
2020-12-05 20:29:32 +0800

    新增阿里云平台基准代码

---
+++ 052c doc/052c code/aliyun_code/model/test.py
@@ -0,0 +1,14 @@
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torch.autograd import Variable
+# import numpy as np
+# import math
+# import os
+
+index = []
+a = [0]
+for i in range (0, 10):
+    a = list((i, ))
+    index.append(a)
+print(index[:][:])
