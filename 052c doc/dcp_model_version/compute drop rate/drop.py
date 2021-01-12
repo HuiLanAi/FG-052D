6e7fb2e11fcb667b58b0fca9a82a23fbaca2d46a
HuiLanAi <310-we-aaa-1@163.com>
2021-01-12 15:07:09 +0800

    一些新的实验数据更新

---
+++ 052c doc/dcp_model_version/compute drop rate/drop.py
@@ -0,0 +1,48 @@
+import os
+file_name = "absmean10.txt"
+
+
+def gen_drop_channel(f_abs_mean, ratio, serial):
+    abs_mean = []
+    drop_index = []
+
+    f = open(f_abs_mean, "r")
+    for num in f.readlines():
+        abs_mean.append(float(num))
+    f.close()
+    drop_num = (int)(ratio * len(abs_mean))
+
+    for i in range (drop_num):
+        index = abs_mean.index(min(abs_mean))
+        drop_index.append(index)
+        abs_mean[index] = 1000
+    
+    if serial != 'fc': 
+        dst_name = "L" + str(serial) + "_drop.txt"
+    else:
+        dst_name = "fc_drop.txt"
+    
+    f = open(dst_name, "w")
+    for i in range (len(drop_index)):
+        f.write(str(drop_index[i]) + '\n')
+    f.close()
+
+
+
+def load_ratio(f_ratio):
+    ratio = []
+    f = open(f_ratio, "r")
+    for num in f.readlines():
+        ratio.append(float(num))
+    f.close()
+    return ratio
+
+
+
+
+ratio = load_ratio("droprate.txt")
+
+# for i in range (10):
+#     f_abs_mean = "absmean" + str(i+1) + ".txt"
+
+gen_drop_channel("abs_10.txt", 0.75, 10)
